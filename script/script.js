95d777393f70f298998c11030bed3bd163c13d7a
MelmanArtem <122752598+MelmanArtem@users.noreply.github.com>
2023-10-11 18:27:03 +0300

    add search

---
+++ script/script.js
@@ -0,0 +1,7 @@
+//Search form
+function openModal() {
+    document.getElementById("overlay").style.top = "0px";
+}
+function closeModal() {
+    document.getElementById("overlay").style.top = "-100vh";
+}
